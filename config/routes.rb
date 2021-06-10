49c2bb234a7bec07e18a8bc35b0a5ec3168a744b
pierrebollack <pierre.bollack@edhec.com>
2021-06-10 17:16:29 +0200

    add styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
+  root to: "pages#home"
 end
