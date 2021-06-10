74c0a570554e3600b10a0cfa723281176ffbc0e1
pierrebollack <pierre.bollack@edhec.com>
2021-06-10 14:11:15 +0200

    detroy bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -19,10 +19,9 @@ class BookmarksController < ApplicationController
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
     @bookmark.destroy
-
-    # no need for app/views/restaurants/destroy.html.erb
-    redirect_to bookmarks_path
+    redirect_to list_path(@list)
   end
 
   private
